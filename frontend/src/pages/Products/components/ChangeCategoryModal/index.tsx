8633fb6851da15353ae68dfbaa0d4591fc275cbf
Nurtimax <maksatbeknurtilek@gmail.com>
2024-10-04 11:19:35 +0600

    fix it typerror

--- frontend/src/pages/Products/components/ChangeCategoryModal/index.tsx
+++ frontend/src/pages/Products/components/ChangeCategoryModal/index.tsx
@@ -1,8 +1,8 @@
-import React, {FC, useEffect, useState} from 'react';
-import {Modal, Select, Spin} from "antd";
-import {ProductType} from "../../../../types/product.type";
-import {CategoryType} from "../../../../types/Category.type";
-import {api} from "../../../../utils/api";
+import React, { FC, useEffect, useState } from 'react';
+import { Modal, Select, Spin } from 'antd';
+import { ProductType } from '../../../../types/product.type';
+import { CategoryType } from '../../../../types/Category.type';
+import { api } from '../../../../utils/api';
 
 type ChangeCategoryModalProps = {
   handleClose: () => void;
@@ -10,23 +10,35 @@ type ChangeCategoryModalProps = {
   onOk: (categoryId: string, productId: string) => Promise<void>;
 };
 
-const ChangeCategoryModal: FC<ChangeCategoryModalProps> = ({handleClose, product, onOk}) => {
+const ChangeCategoryModal: FC<ChangeCategoryModalProps> = ({
+  handleClose,
+  product,
+  onOk,
+}) => {
   const [selectCategory, setCategory] = useState<string>('');
-  const [categories, setCategories] = useState<{label: string, value: string}[]>([]);
+  const [categories, setCategories] = useState<
+    { label: string; value: string }[]
+  >([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    api.get<CategoryType[]>(`/category`)
-      .then(res => {
-        const filteredCategories = res.data.filter(cat => cat._id !== product.category._id);
-        const items = filteredCategories.map(el => ({
+    api
+      .get<CategoryType[]>(`/category`)
+      .then((res) => {
+        const filteredCategories = res.data.filter(
+          (cat) => cat._id !== product?.category?._id
+        );
+        const items = filteredCategories.map((el) => ({
           label: el.name,
-          value: el._id
-        }))
+          value: el._id,
+        }));
         setCategories(items);
       })
-      .catch(err => alert(err))
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      })
       .finally(() => {
         setLoading(false);
       });
@@ -36,27 +48,26 @@ const ChangeCategoryModal: FC<ChangeCategoryModalProps> = ({handleClose, product
     onOk(selectCategory, product._id);
   };
 
-
   return (
     <Modal
       open
       onCancel={handleClose}
       onOk={handleOk}
       okButtonProps={{
-        disabled: !selectCategory
+        disabled: !selectCategory,
       }}
       title={`Изменить категорию продукта - ${product.article}`}
     >
       <Spin spinning={loading}>
-        {
-          product.category ?
-            <p>Нынешняя категория - {product.category.name}</p>
-            : <p>У данного продукта отсутствует категория</p>
-        }
+        {product.category ? (
+          <p>Нынешняя категория - {product.category.name}</p>
+        ) : (
+          <p>У данного продукта отсутствует категория</p>
+        )}
 
         <Select
-          style={{width: '100%'}}
-          onChange={v => setCategory(v)}
+          style={{ width: '100%' }}
+          onChange={(v) => setCategory(v)}
           options={categories}
           size="large"
           value={selectCategory}
