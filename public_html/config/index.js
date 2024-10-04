bf3884490f9a870e6252fd4ac3bf7427b0c1f303
Nurtimax <maksatbeknurtilek@gmail.com>
2024-10-04 23:50:03 +0600

    remove e

--- public_html/config/index.js
+++ public_html/config/index.js
@@ -20,7 +20,7 @@ async function fetchWithAuth({ url, method = 'GET', body, params }) {
     const response = await fetch(`${url}${urlParam}`, {
       body,
       headers: {
-        Authorization: `Bearer ${token}`, // Authorization header with toke
+        Authorization: `Bearer ${token}`, // Authorization header with tok
         'Content-Type': 'application/json', // adjust as needed
       },
     });
